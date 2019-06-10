6560d526d78c113fbbeb5419ea8efba715d17cd5
Welford <John.Welford@wsp.com>
2019-06-11 00:15:43 +1200

    added horrible hacks to make functional on IE

--- js/inlineSvg.js
+++ js/inlineSvg.js
@@ -1,11 +1,23 @@
-if (window.NodeList && !NodeList.prototype.forEach) { // polyfill for browsers not supporting forEach on NodeLists
+if (window.NodeList && !NodeList.prototype.forEach) { // polyfill for browsers not supporting forEach on NodeLists (IE and edge)
   NodeList.prototype.forEach = Array.prototype.forEach;
+}
+
+if (!Object.getOwnPropertyDescriptor(Element.prototype,'classList')){ // polyfill for IE not supporting classList on SVG elements
+  if (HTMLElement&&Object.getOwnPropertyDescriptor(HTMLElement.prototype,'classList')){
+    Object.defineProperty(Element.prototype,'classList',Object.getOwnPropertyDescriptor(HTMLElement.prototype,'classList'));
+  }
+}
+
+var ua = window.navigator.userAgent;
+if (/MSIE|Trident/.test(ua)) {
+  var sections = document.getElementsByTagName('section');
+  for (var i = 0; i < sections.length; i++) sections[i].style.overflow = 'hidden';
 }
 
 document.querySelectorAll('img[src$=".svg"]').forEach(function(e) {
   SVGInjector(e, {each: function(svg){
     svg.querySelectorAll('*').forEach(function(svgEl) {
-      svgEl.classList.add('mute'); // mute all elements initially
+      if(svgEl.classList) svgEl.classList.add('mute'); // mute all elements initially
     });
     // fix give the svg a max width and height but make it responsive
     var wrapper = document.createElement('div'); // add a wrapper to the svg
@@ -16,11 +28,19 @@ document.querySelectorAll('img[src$=".svg"]').forEach(function(e) {
     wrapper.classList.add('svgWrapper');
     svg.removeAttribute("height"); // remove svg fixed size to make it responsive
     svg.removeAttribute("width");
+
+    // horrible IE hacks to follow...
+    if (/MSIE|Trident/.test(ua)) {
+      wrapper.style.position = 'relative';
+      wrapper.style.paddingBottom = (100*parseInt(wrapper.style.maxHeight,10)/parseInt(wrapper.style.maxWidth,10))+'%';
+      wrapper.style.maxWidth = null;
+      svg.style.position = 'absolute';
+    }
   }});
 });
 
 document.querySelectorAll('.figEm').forEach(function(e) { // for all text with the figEm class...
-  e.addEventListener("mouseover", function() {  // add a mouse hover event
+  e.addEventListener("mouseover", function() { // add a mouse hover event
     e.className.split(/\s+/).forEach(function(classEm) { // for each other class in the moused text
       if (classEm!='figEm') document.querySelectorAll('.'+classEm).forEach(function(emEl) { // find all objects to emphasise
         emEl.classList.add('active');
